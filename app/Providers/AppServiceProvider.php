e242960997a7d11fdb0cfeb07cc62b9a8bad3a9c
Zemen Heliso <150444726+Zeda-HH@users.noreply.github.com>
2026-06-13 13:37:19 +0300

    Force HTTPS unconditionally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 }
